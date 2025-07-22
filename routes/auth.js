ba1052fcfc8e76ba482638c47320619709bc9d45
Sikho Mzoboshe <134971629+itsaot@users.noreply.github.com>
2025-07-22 13:20:13 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { register, login, getUser } = require("../controllers/userController");
 const { auth } = require("../middleware/auth");
 
-router.post("/register", register);
-router.post("/login", login);
 router.get("/me", auth, getUser);
 
 module.exports = router;
