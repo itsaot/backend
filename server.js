ac122903521289e12998160ebb5d21cdb1afa061
Sikho Mzoboshe <134971629+itsaot@users.noreply.github.com>
2025-07-22 18:59:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
