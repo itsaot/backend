1068e2dce0c48b03be165db8fd97c402f3f959fc
Sikho Mzoboshe <134971629+itsaot@users.noreply.github.com>
2025-07-23 07:52:56 +0200

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,46 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  type: { type: String, enum: ["physical","verbal","cyber"], required: true },
-  content: { type: String, required: true },
-  adviceRequested: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+  type: {
+    type: String,
+    enum: ["physical", "verbal", "cyber"], // abuse type
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  tags: [{
+    type: String,
+  }],
+  category: {
+    type: String,
+  },
+  adviceRequested: {
+    type: Boolean,
+    default: false,
+  },
+  escalated: {
+    type: Boolean,
+    default: false,
+  },
+  escalationDetails: {
+    reportedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    reportedAt: { type: Date },
+  },
+  isAnonymous: {
+    type: Boolean,
+    default: false,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
